4ffc4c5535730313d1fcd6631827987c36f19868
Doron Segal <doron2402@gmail.com>
2025-07-04 11:57:47 +0300

    feat: more tests
    adding more unit tests

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const port = process.env.PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
 
 app.get('/', (req, res) => {
+  console.log(`${req.method} ${req.url}`);
   res.send('Hello World');
+})
+.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  res.status(404).send('Not Found');
+})
+.use((err, req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(port, host, () => {
